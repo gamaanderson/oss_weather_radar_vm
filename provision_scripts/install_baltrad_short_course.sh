e21f6006c639ca9f435b7ece32e2fb09d0f03a31
Gama Anderson <gamaanderson92@gmail.com>
2017-08-17 08:01:01 +0200

    add baltrad2wradlib

--- provision_scripts/install_baltrad_short_course.sh
+++ provision_scripts/install_baltrad_short_course.sh
@@ -10,5 +10,5 @@ cd tmp
 git clone --depth=1 https://github.com/DanielMichelson/baltrad_short_course.git
 mv baltrad_short_course ../.
 git clone --depth=1 https://github.com/DanielMichelson/baltrad2wradlib.git
-mv baltrad2radlib ../.
+mv baltrad2wradlib ../.
 cd ~
